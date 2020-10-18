8977e39c47b4b17d57d36128cccf2a54caff10a2
Mateja <mail@matejamaric.com>
2020-10-18 23:12:49 +0200

    more views with tables

--- resources/views/pages/reservations.blade.php
+++ resources/views/pages/reservations.blade.php
@@ -4,7 +4,7 @@
 <div class="table-responsive">
     <table class="table table-striped table-bordered"><!-- table-hover -->
         <thead class="thead-dark">
-            <tr><th>Operator</th><th>From</th><th>To</th><th>Special Callsign</th><th>Frequencies</th><th>QSO</th></tr>
+            <tr><th>ID</th><th>Approved</th><th>Operator Callsign</th><th>QSO</th><th>From</th><th>To</th><th>Frequencies</th><th>Modes</th><th>Special Callsign</th><th>Operator Name</th><th>Operator Email</th><th>Operator Phone</th><th>Actions</th></tr>
         </thead>
         <tbody>
         </tbody>
