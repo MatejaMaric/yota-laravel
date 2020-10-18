1f1cc4ae5facaac11a9e95dcc4cdfd3ae67dd788
Mateja <mail@matejamaric.com>
2020-10-18 23:28:14 +0200

    add pictures

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+    <img src="/imgs/camp.png" alt="YOTA camp"/>
+    <img src="/imgs/yota.jpg" alt="YOTA"/>
+
+    <h1>Sponsors: </h1>
+    <img src="/imgs/4o3a.png" alt="4o3a"/>
+    <img src="/imgs/spiderbeam.gif" alt="Spiderbeam"/>
 @endsection()
