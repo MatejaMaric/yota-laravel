74c8b5a9fba331e0151a05814cd1f9b35079a87f
Mateja <mail@matejamaric.com>
2020-10-19 20:01:23 +0200

    Add title to posts migration

--- database/migrations/2020_10_18_142620_create_posts_table.php
+++ database/migrations/2020_10_18_142620_create_posts_table.php
@@ -15,6 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 255);
             $table->string('author', 255)->nullable();
             $table->longText('text');
             $table->timestamps();
