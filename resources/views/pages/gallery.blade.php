d277b1b4d92bbacdf8533ded58186564b3614cf0
Mateja <mail@matejamaric.com>
2020-10-28 00:33:36 +0100

    Bootstrap mobile margin fix.

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row">
-        <img class="col-lg-6" src="/imgs/camp.png" alt="YOTA camp"/>
-        <img class="col-lg-6" src="/imgs/yota.jpg" alt="YOTA"/>
+        <img class="col-lg-6 mb-3 mb-lg-0" src="/imgs/camp.png" alt="YOTA camp"/>
+        <img class="col-lg-6 mb-3 mb-lg-0" src="/imgs/yota.jpg" alt="YOTA"/>
     </div>
 @endsection()
