82498e1687359b5862ed8032f9fe1da6350a8703
Mateja <mail@matejamaric.com>
2020-11-15 12:15:34 +0100

    Don't use Laravel Mix for now...

--- resources/js/reserve.js
+++
@@ -1,9 +0,0 @@
-jQuery('select#special-call').change(setCallDesc);
-jQuery(document).ready(setCallDesc);
-
-function setCallDesc() {
-    sign = jQuery('select#special-call').first().val();
-    jQuery.get('/special-calls/show/' + sign, function (data, status) {
-        jQuery('div#call-desc').html(data);
-    });
-}
