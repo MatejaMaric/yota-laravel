ef96b4953e0e5c89d95b6c6eb4b2fc2c3d9c9464
Mateja <mail@matejamaric.com>
2021-03-14 19:59:52 +0100

    Start work on `activities` component.

--- resources/js/components/activities.vue
+++ resources/js/components/activities.vue
@@ -1,9 +1,31 @@
 <template>
-  
+  <div>
+    <call-sign-filter></call-sign-filter>
+
+    <div class="table-responsive mt-2">
+        <table class="table table-striped table-bordered" style="white-space:nowrap;">
+            <thead class="thead-dark">
+                <tr>
+                  <th>Operator</th>
+                  <th>From</th>
+                  <th>To</th>
+                  <th>Special Callsign</th>
+                  <th>Frequencies</th>
+                  <th>Modes</th>
+                  <th>QSO</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+  </div>
 </template>
 
 <script>
+import callSignFilter from './call-sign-filter.vue';
+
 export default {
-  
+  components: { callSignFilter }
 }
 </script>
--- resources/views/pages/activities.blade.php
+++ resources/views/pages/activities.blade.php
@@ -5,33 +5,7 @@
 @section('navbar', View::make('inc.navbar'))
 
 @section('content')
-<input type="hidden" name="csrf-token" content="{{ csrf_token() }}">
 
-<call-sign-filter></call-sign-filter><br>
+<activities-view></activities-view>
 
-<label for="call-sign">Filter by special callsign: </label>
-<select id="call-sign">
-    <option value="all">All</option>
-    @if (count($signs) > 0)
-        @foreach ($signs as $sign)
-            <option value="{{ $sign->sign }}">{{ $sign->sign }}</option>
-        @endforeach
-    @endif
-</select>
-
-<div id="sign-desc-div"></div>
-
-<div class="table-responsive mt-2">
-    <table id="ajax-table" class="table table-striped table-bordered" style="white-space:nowrap;">
-        <thead class="thead-dark">
-            <tr><th>Operator</th><th>From</th><th>To</th><th>Special Callsign</th><th>Frequencies</th><th>Modes</th><th>QSO</th></tr>
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
 @endsection()
-
-@section('scripts')
-    <script src="{{ asset('js/activities.js') }}"></script>
-@endsection
