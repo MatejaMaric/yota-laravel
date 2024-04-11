45c2d724f8bdbc8b4b2e1a465c8a92a08801f1a2
Mateja <mail@matejamaric.com>
2024-04-11 22:28:30 +0200

    feat: nix flake and update php to v8.3

---
+++ derivative.nix
@@ -0,0 +1,10 @@
+{ php83 }: php83.buildComposerProject (finalAttrs: {
+    pname =  "yota-laravel";
+    version = "2.0.0";
+    src = ./.;
+    php = php83.buildEnv {
+        extensions = ( { enabled, all }: enabled );
+    };
+    composerLock = ./composer.lock;
+    vendorHash = "sha256-vYuWiX3YxS6ZZ3ngsYDuR6ydggBBwBG8K+KRBP8UqrA=";
+})
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description = "Nix Flake package for deploying Serbian YOTA Reservation Laravel web application";
+    inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
+    outputs = { self, nixpkgs }:
+    let
+        pkgName = "yota-laravel";
+        supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+        forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+        nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in {
+        overlays.default = (final: prev: {
+            ${pkgName} = prev.callPackage ./derivative.nix {};
+        });
+        packages = forAllSystems (system: {
+            ${pkgName} = nixpkgsFor.${system}.callPackage ./derivative.nix {};
+            default = nixpkgsFor.${system}.callPackage ./derivative.nix {};
+        });
+    };
+}
