4eda6730bacb18984794c27f5514d3f7d744a8eb
Mateja <mail@matejamaric.com>
2024-04-28 20:55:43 +0200

    patch(flake): switch to fixed output derivation

--- derivation.nix
+++ derivation.nix
@@ -1,15 +1,39 @@
-{ php83 }: php83.buildComposerProject (finalAttrs: {
+{ stdenvNoCC, php83, lib }: stdenvNoCC.mkDerivation (finalAttrs:
+let
+    php' = php83.withExtensions ({ enabled, all }: enabled );
+in {
     pname =  "yota-laravel";
     version = "2.0.0";
+
     src = ./.;
-    php = php83.buildEnv {
-        extensions = ( { enabled, all }: enabled );
-    };
-    composerLock = ./composer.lock;
-    vendorHash = "sha256-vYuWiX3YxS6ZZ3ngsYDuR6ydggBBwBG8K+KRBP8UqrA=";
-    postInstall = ''
+
+    nativeBuildInputs = [
+        php'
+        php'.packages.composer
+    ];
+
+    buildInputs = [
+        php'
+    ];
+
+    COMPOSER_CACHE_DIR = "/dev/null";
+    COMPOSER_MIRROR_PATH_REPOS = "1";
+    COMPOSER_HTACCESS_PROTECT = "0";
+    COMPOSER_DISABLE_NETWORK = "0";
+
+    buildPhase = ''
+        composer --no-ansi --no-interaction --no-dev --no-plugins --no-scripts install
         php artisan config:cache
         php artisan route:cache
         php artisan view:cache
+    '';
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = lib.fakeSha256;
+
+    installPhase = ''
+        mkdir $out
+        cp -r . $out
     '';
 })
